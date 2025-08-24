a1a7ee5e20b52de129e0ff28c3ee7e7b77418895
Tim Miller <tim@rocket5studios.com>
2025-08-23 23:56:30 -0400

    Enhance headshot hover effect in Index.tsx with transition and scaling on hover

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -28,7 +28,7 @@ export default function Index() {
           }}
         >
           {/* Headshot with background circle */}
-          <div className="relative w-32 h-32 sm:w-40 sm:h-40 lg:w-[188px] lg:h-[188px]">
+          <div className="relative w-32 h-32 sm:w-40 sm:h-40 lg:w-[188px] lg:h-[188px] transition-transform duration-300 ease-in-out hover:scale-105 cursor-pointer">
             <div
               className="absolute w-32 h-32 sm:w-40 sm:h-40 lg:w-[188px] lg:h-[188px] rounded-full"
               style={{
